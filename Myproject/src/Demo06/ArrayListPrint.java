e5db736b4db4521b7e3d71055e7ef7c73c47f65f
Kana <kana350594945@gmail.com>
2020-09-27 20:30:54 +0900

    version5-for ArrayList excercise

---
+++ Myproject/src/Demo06/ArrayListPrint.java
@@ -0,0 +1,50 @@
+package Demo06;
+
+import java.util.ArrayList;
+
+/*
+ * 题目：
+ * 定义以指定格式打印集合的方法（Arraylist类型作为参数，）
+ * 使用{}扩起集合，使用@分隔每个元素。格式参照{元素@元素@元素}
+ * system.put.println(list); [10,20,30]
+ * printArrayList(list); {10@20@30}
+ */
+
+public class ArrayListPrint {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<>();
+		
+		list.add("ちびまる子");
+		list.add("どらえもん");
+		list.add("シャーマンキング");
+		
+		System.out.println(list);
+		
+		printArrayList(list);
+		
+		/*定义方法的三要素：
+		 * 返回值类型：只是打印而已，没有运算，没有结果，so，用void；
+		 * 方法名称：printArrayList；
+		 * 参数列表：ArrayList
+		 */
+	}
+	
+	public static void printArrayList(ArrayList<String> list) {
+		// {10@20@30}
+		
+		System.out.print("{");
+		
+		for(int i=0;i<list.size();i++) {
+			String name=list.get(i);
+			
+			if(i==list.size()-1) {
+				System.out.println(name+"}");
+			}
+			else {
+			System.out.print(name+"@");
+			}
+		}
+	}
+}
